599ce2629d32dfb4c5cadf1f44bebd8084147249
Rajeev Kumar <rajeev.kumar3@myntra.com>
2021-10-11 07:21:47 +0530

    minor changes

--- MongoDB-03-March-2020/src/main/java/com/example/demo/MongoDbApplication.java
+++ MongoDB-03-March-2020/src/main/java/com/example/demo/MongoDbApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import com.mongodb.MongoClientOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -12,6 +14,11 @@ public class MongoDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoDbApplication.class, args);
+	}
+
+	@Bean
+	public MongoClientOptions mongoOptions() {
+		return MongoClientOptions.builder().connectTimeout(2).maxWaitTime(1).build();
 	}
 
 }
